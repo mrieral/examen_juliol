2d859bfc425d0e6af333bc6258e9076c871480c0
nom <correu>
2020-07-03 16:47:00 +0200

    Init

---
+++ examen_julio/src/examen_julio/NombreAlumno.java
@@ -0,0 +1,13 @@
+package examen_julio;
+
+public class NombreAlumno {
+
+	private String nombre="Tu Nombre";
+	
+	public NombreAlumno() {	
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+}
